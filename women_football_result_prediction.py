28e0fb869a64e4240781cdb9b801f10d87857448
Emmanuel Boniface <bonifacechosen100@gmail.com>
2023-06-21 21:30:56 +0100

    Added new files and modified code

--- women_football_result_prediction.py
+++ women_football_result_prediction.py
@@ -1,29 +1,49 @@
 import streamlit as st
 import joblib
-import category_encoders
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 # Load the trained models
-home_model = joblib.load("home_score_model.pkl")
-away_model = joblib.load("away_score_model.pkl")
+home_model = joblib.load("/home/boniface/Desktop/Projects/Women-Football-Result-Prediction/home_model.pkl")
+away_model = joblib.load("/home/boniface/Desktop/Projects/Women-Football-Result-Prediction/away_model.pkl")
+
+# Load the features
+features = pd.read_csv('/home/boniface/Desktop/Projects/Women-Football-Result-Prediction/features.csv')
+
+# Create dictionaries for storing the mappings
+mappings = {}
+
+# Encode all columns
+for col in features.columns:
+    # Create a label encoder
+    encoder = LabelEncoder()
+    
+    # Fit and transform the column
+    features[col] = encoder.fit_transform(features[col])     
+    
+    # Create a dictionary mapping the categories to their encoded values
+    mapping = dict(zip(encoder.classes_, encoder.transform(encoder.classes_)))
+    
+    # Store the mapping dictionary
+    mappings[col] = mapping
 
 # Create a function to encode features
-def encode_features(features):
-    features_df = pd.DataFrame([features], columns=["home_team", "away_team", "tournament", "city", "country", "neutral", "Year"])
+def encode_features(features, mappings):
+    columns_list = features.columns
+    encoded_input = []
 
-    # Frequency Encoding
-    features_df_copy = features_df.copy()
-    columns_list = features_df.columns
+    # Iterate over each item in the input array
+    for i, item in enumerate(features.values[0]):
+        # Check if the item's column exists in the mappings dictionary
+        if columns_list[i] in mappings:
+            # Get the encoded value for the category using the mappings dictionary
+            encoded_value = mappings[columns_list[i]].get(item, -1)  # Use -1 as a default value if the category is not found in mappings
+            encoded_input.append(encoded_value)
+        else:
+            encoded_input.append(item)  # If the column is not categorical, keep the original value
 
-    # Encode all columns
-    for col in columns_list:
-        # Encoder
-        encoder = category_encoders.CountEncoder(cols=col, normalize=True)
-
-        # Fit and transform
-        features_df[col] = encoder.fit_transform(features_df[col])
-
-    return features_df
+    input_row = pd.DataFrame([encoded_input], columns=['home_team', 'away_team', 'tournament', 'city', 'country','neutral','Year'])
+    return input_row
 
 # Streamlit app
 def main():
@@ -41,14 +61,14 @@ def main():
     
     # Predict button
     if st.button("Predict"):
-        features = [home_team, away_team, tournament, city, country, neutral, year]
-        features_df = encode_features(features)
+        input_features = [home_team, away_team, tournament, city, country, neutral, year]
+        encoded_input = encode_features(pd.DataFrame([input_features], columns=['home_team', 'away_team', 'tournament', 'city', 'country','neutral','Year']), mappings)
 
         # Predict home score
-        home_score = home_model.predict(features_df)
+        home_score = home_model.predict(encoded_input)
 
         # Predict away score
-        away_score = away_model.predict(features_df)
+        away_score = away_model.predict(encoded_input)
 
         # Determine the winner
         winner = home_team if home_score[0] > away_score[0] else away_team
